d2e24e12b70260054616363733ab277e721912f5
idilia wheat <lillyw54@gmail.com>
2024-01-21 00:23:40 -0600

    rewrite and clean up

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const express = require('express');
-const util = require('util');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,41 +8,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+        // Define paths for the HTML files
+const indexPath = path.join(__dirname, './public/index.html');
+const notesPath = path.join(__dirname, './public/notes.html');
+const dbPath = './db/db.json';
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
-);
+         // Serve index.html at the root path
+app.get('/', (req, res) => res.sendFile(indexPath));
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+        // Serve notes.html at the '/notes' path
+app.get('/notes', (req, res) => res.sendFile(notesPath));
 
-app.get('/api/notes', (req, res) => {
-    readFileAsync('./db/db.json').then(function (data) {
-        notes = [].concat(JSON.parse(data))
+        // API end point to get the note
+app.get('/api/notes', async (req, res) => {
+    try {
+        const data = await fs.readFile(dbPath);
+        const notes = JSON.parse(data);
         res.json(notes);
-    })}
-);
+    } catch (error) {
+        console.error('Error reading notes:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.post('/api/notes', (req, res) => {
-    const note = req.body;
-    readFileAsync('./db/db.json').then(function (data) {
-        notes = [].concat(JSON.parse(data));
+        // API endpoint for a new note
+app.post('/api/notes', async (req, res) => {
+    try {
+        const note = req.body;
+        const data = await fs.readFile(dbPath);
+        const notes = JSON.parse(data);
         note.id = notes.length + 1;
         notes.push(note);
-        return notes
-    }).then(function (notes) {
-        writeFileAsync('./db/db.json', JSON.stringify(notes))
+        await fs.writeFile(dbPath, JSON.stringify(notes));
         res.json(note);
-    })}
-);
+    } catch (error) {
+        console.error('Error adding note:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-  });
+        // Serve index.html for other paths
+app.get('*', (req, res) => res.sendFile(indexPath));
 
-app.listen(PORT, () =>
-    console.log(`Listening at http://localhost:${PORT}`)
-);
+        // Starting the server
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
